49c88aef0519e5874327385103e6a13bc3e759ef
Lukasz <syljon2@gmail.com>
2019-05-19 18:42:23 +0200

    recreate HomePage,EventBoxList and create Header, CityButtonList

--- src/API/API.js
+++ src/API/API.js
@@ -3,12 +3,10 @@ export function getEvents() {
     method: "Get"
   })
     .then(response => response.json())
-    .then(response => Object.keys(response).map(res => response[res]));
+    .then(response =>
+      Object.keys(response).map(res => {
+        response[res].id = res;
+        return response[res];
+      })
+    );
 }
-
-//   let fetchEvents = [];
-//   for (let key in response) {
-//     response[key].id = key;
-//     fetchEvents.push(response[key]);
-//   }
-//   this.setState({ events: fetchEvents, loading: false });
